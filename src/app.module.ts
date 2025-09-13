b906938f28889c38bbfd546caedb6299e6ce6661
Lucas Lima <lucasvinilima15@gmail.com>
2025-09-13 12:24:24 -0300

    feat: add PrismaModule and PrismaService for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     UserModule
   ],
   controllers: [AppController],
-  providers: [PrismaService],
 })
 export class AppModule {}
