fae5c1e63264eca396ee8a252cce031512804031
Lucas Lima <lucasvinilima15@gmail.com>
2025-09-14 16:15:58 -0300

    feat: enhance UserController with Swagger decorators for create user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
-import { Prisma, User } from "@prisma/client";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { ApiBody, ApiCreatedResponse, ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { User } from "./entities/user.entity";
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Criar um novo usuário' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiCreatedResponse({ type: User, description: 'Usuário criado com sucesso.' })
   async create(@Body() data: CreateUserDto): Promise<User> {
     return this.userService.createUser(data);
   }
